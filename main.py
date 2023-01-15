635600fb256e7a79d976473d7c69ff193080a141
holgerlindholm <holger1234lindholm1234@gmail.com>
2023-01-15 17:15:12 +0100

    Update and rename KNAP.py to main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import RPi.GPIO as GPIO
 import subprocess
 
+#Initialisering af knap + LED
 GPIO.setwarnings(False)
 knap_GPIO = 23
 LED_GPIO = 24
@@ -12,15 +13,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(knap_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LED_GPIO,GPIO.OUT)
 
-
 state = []
-
 def signal_handler(sig, frame):
     GPIO.cleanup()
     sys.exit(0)
 
 def button_pressed_callback(channel):
-        
     if len(state) == 0: #Når man trykket på knappen første gang er længden af 'state' 0, og dette vil køre:
         state.extend([1]) #'state' forlænges en gang
         GPIO.output(LED_GPIO,GPIO.HIGH) #LED'en tændes
@@ -30,7 +28,6 @@ def button_pressed_callback(channel):
         GPIO.output(LED_GPIO,GPIO.LOW) # LED'en slukkes
 
 GPIO.add_event_detect(knap_GPIO, GPIO.FALLING, callback=button_pressed_callback, bouncetime=200)
-#init() #Initialiserer knap + LED port
 
 while True: #Main loop
     #'state' vil have en længde på 1, når LED'en er tændt
@@ -49,6 +46,3 @@ while True: #Main loop
         sub3.kill()
         del state[0:3] #Alt i 'state' fjernes, og man kan tænde LED'en igen (og dermed starte scripts igen) ved at trykke igen
         print(state)
-
-#signal.signal(signal.SIGINT, signal_handler)
-#signal.pause()
